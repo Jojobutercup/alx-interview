29764af251717185ec6f04ad6dd615bdc8c8a585
jojobutercup <jojobutercup@gmail.com>
2023-04-23 17:27:31 -0700

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+This is a module that gives the Pascal's triangle of a number.
+"""
+
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing the Pascal's triangle of n
