f8187b5c6c35a38814b4ab5f9c2f426a6f89d0df
jojobutercup <jojobutercup@gmail.com>
2023-05-01 02:38:35 +0000

    opened boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-
 """
 You have n number of locked boxes in front of you.
 Each box is numbered sequentially from 0 to n - 1 and each box may
 contain keys to the other boxes.
 """
-
-
 def canUnlockAll(boxes):
-    # Create a list to keep track of which boxes have been opened
-    opened_boxes = [False] * len(boxes)
-    opened_boxes[0] = True  # The first box is already opened
-
-    # Create a list to keep track of the keys that we have
-    keys = [0]  # We start with the keys in the first box
-
-    # While we have keys to use
-    while keys:
-        # Get the next key
-        key = keys.pop()
-
-        # Try to open all the boxes that this key can open
-        for box in boxes[key]:
-            if not opened_boxes[box]:
-                opened_boxes[box] = True
-                keys.append(box)
-
-    # Check if all boxes have been opened
-    return all(opened_boxes)
-
+    """
+    Determine if all the boxes can be opened.
+    Args:
+        boxes (list): A list of lists containing keys to other boxes.
+    Returns:
+        boolen: True if all boxes can be opened, False if they cannot be opened.
+    """
+    if not boxes or type(boxes) is not list:
+        return False
+    unlocked = [0]
+    for n in unlocked:
+        for keys in boxes[n]:
+            if keys not in unlocked and keys < len(boxes):
+                unlocked.append(keys)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
