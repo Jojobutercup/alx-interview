0d6dc892686f235c46770ddf16a5e7d0c80ee1db
jojobutercup <jojobutercup@gmail.com>
2023-05-19 05:59:26 +0100

    UFT8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,46 +2,58 @@
 
 
 def validUTF8(data):
-    # Variable to track the number of expected bytes for the next character
-    num_bytes = 0
-
-    # Iterate through each integer in the data set
-    for num in data:
-        # Check if it is a continuation byte (starts with 10)
-        if num >> 6 == 0b10:
-            # If there are no expected bytes, it is an invalid encoding
-            if num_bytes == 0:
-                return False
-            # Decrement the number of expected bytes
-            num_bytes -= 1
-        else:
-            # Check the number of bytes for the next character
-            if num_bytes > 0:
-                return False
-            # Determine the number of expected bytes
-            # based on the first few bits
-            if num >> 7 == 0b0:
-                num_bytes = 0  # 1 byte character
-            elif num >> 5 == 0b110:
-                num_bytes = 1  # 2 byte character
-            elif num >> 4 == 0b1110:
-                num_bytes = 2  # 3 byte character
-            elif num >> 3 == 0b11110:
-                num_bytes = 3  # 4 byte character
+    """Checks if a list of integers are valid UTF-8 codepoints.
+    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
+    """
+    skip = 0
+    n = len(data)
+    for i in range(n):
+        if skip > 0:
+            skip -= 1
+            continue
+        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
+            return False
+        elif data[i] <= 0x7f:
+            skip = 0
+        elif data[i] & 0b11111000 == 0b11110000:
+            # 4-byte utf-8 character encoding
+            span = 4
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
             else:
-                return False  # Invalid starting byte
-
-    # If there are remaining expected bytes, it is an invalid encoding
-    return num_bytes == 0
-
-
-if __name__ == "__main__":
-    data = [65]
-    print(validUTF8(data))
-
-    data = [80, 121, 116, 104, 111, 110, 32, 105, 115,
-            32, 99, 111, 111, 108, 33]
-    print(validUTF8(data))
-
-    data = [229, 65, 127, 256]
-    print(validUTF8(data))
+                return False
+        elif data[i] & 0b11110000 == 0b11100000:
+            # 3-byte utf-8 character encoding
+            span = 3
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif data[i] & 0b11100000 == 0b11000000:
+            # 2-byte utf-8 character encoding
+            span = 2
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        else:
+            return False
+    return True
