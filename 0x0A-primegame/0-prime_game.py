91235aa25911c2b30451c37eafc6c2d93f074d6a
jojobutercup <jojobutercup@gmail.com>
2023-06-29 16:16:59 +0100

    interview

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def isWinner(x, nums):
+    # Your implementation for determining the winner goes here
+    pass
+
+if __name__ == "__main__":
+    # Example test case
+    x = 3
+    nums = [4, 5, 1]
+    print("Winner: {}".format(isWinner(x, nums)))
+
